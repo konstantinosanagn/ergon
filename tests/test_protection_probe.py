b627076e6773217b06cb5d02c86f700f274db9fc
kanagn <kwnstantinosanagn@gmail.com>
2026-09-08 22:51:24 -0400

    Revert "test: temporary probe to verify branch protection blocks a red PR (#27)"
    This reverts commit 3d0d9c60d3745d586b5742ed16d5108bde56d663.

--- tests/test_protection_probe.py
+++
@@ -1,3 +0,0 @@
-def test_deliberately_failing_probe() -> None:
-    """Temporary: verifies branch protection blocks a red PR. Deleted immediately after."""
-    assert False, "intentional failure to test the merge gate"
